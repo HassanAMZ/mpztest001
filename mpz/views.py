3a32da02c3a7d9349d04cf417eca6bc28870c959
HassanDevelops <59549481+HassanDevelops@users.noreply.github.com>
2020-07-04 18:54:51 +0500

    backend done

--- mpz/views.py
+++ mpz/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+from .models import Mobiledetail
+from .serializers import *
+
+
+@api_view(['GET', 'POST'])
+def mpz_list(request):
+    if request.method == 'GET':
+        data = Mobiledetail.objects.all()
+
+        serializer = MobiledetailSerializer(
+            data, context={'request': request}, many=True)
+
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = MobiledetailSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT', 'DELETE'])
+def mpz_detail(request, pk):
+    try:
+        mobiledetail = Mobiledetail.objects.get(pk=pk)
+    except Mobiledetail.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = MobiledetailSerializer(
+            mobiledetail, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        mobiledetail.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
